aaa3bb4d3c52e0a79b08427c560e1a37430ab070
Séba <seba@seba.gent>
2025-04-21 20:12:39 +0200

    Update const.py

--- custom_components/gent_parking/const.py
+++ custom_components/gent_parking/const.py
@@ -1,5 +1,5 @@
 DOMAIN = "gent_parking"
-DEFAULT_NAME = "Parkeerbezetting Gent"
+DEFAULT_NAME = "Parking Occupancy Ghent"
 API_URL = (
     "https://data.stad.gent/api/explore/v2.1/catalog/"
     "datasets/bezetting-parkeergarages-real-time/records"
